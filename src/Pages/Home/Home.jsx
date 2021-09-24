a2267d356a1f029c3825e25671af23f8f0830730
Bill Zack <bzak954@gmail.com>
2021-09-24 08:11:50 +0000

    add navbar component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+//import navbar component
+import NavBar from '../../Components/NavBar';
 // import dependencies
 import { Link } from "react-router-dom";
 // import css file
@@ -15,45 +17,8 @@ const Home = () => {
     return (
         <>
             <div className="cover">  
-                <nav className="navbar navbar-default headnav">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                        <button 
-                            type="button" 
-                            className="navbar-toggle collapsed" 
-                            data-toggle="collapse" 
-                            data-target="#bs-example-navbar-collapse-1" 
-                            aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-
-                            <Link to="/" className="navbar-brand" style={{color:'black'}}>BILAL's PORTFOLIO</Link>
-
-                        </div>
-
-                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav">
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li>
-                                <Link to="/" className="select">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about">About Me</Link>
-                            </li>
-                            <li>
-                                <Link to="/contacts">Contact Me</Link>
-                            </li>
-                            <li>
-                                <Link to="/projects">My Projects</Link>
-                            </li>
-                        </ul>
-                        </div>
-                    </div>
-                </nav> 
+            
+                <NavBar/>
                 
                 <div className="container firstview">
                     <div className="row">
