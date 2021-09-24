b32373e7b4436074dccff6d0983bb9e6e9798dd1
Bill Zack <bzak954@gmail.com>
2021-09-24 08:20:12 +0000

    create footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="container-fluid text-center" style={{paddingBottom: '20px'}} >
+                    <h2>Portfolio Made By Bill</h2>
+                    <p>Phone: +233 209 688 728</p>
+                    <p style={{color:'grey'}}>Follow me; IG @champ_gobain..Twitter@BillZak3</p>
+                </div>
+        </div>
+    );
+}
+
+export default Footer;
